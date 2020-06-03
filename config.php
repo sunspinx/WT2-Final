39a669435ec39087a5dcbbe299311cfc07c41b85
Peter Bašista <basista.peter1@gmail.com>
2020-06-03 10:07:28 +0200

    config table name fix

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 define("HOSTNAME", "localhost");
 define("USERNAME", "xdomin");
 define("PASSWORD", "edx4even");
-define("DBNAME", "zad7");
+define("DBNAME", "wt2_final");
 define("API_KEY", "KHt9t83cmV6pfcyfSbcB9jyG7TnJMySYT28CUv2jSMN9RrNj");
