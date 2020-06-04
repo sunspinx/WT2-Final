5372364562dd1ebe99ce246b45d4079b6409a40e
Peter Bašista <basista.peter1@gmail.com>
2020-06-04 17:25:46 +0200

    pdf api export

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ require_once 'app/class/core/Db.php';
 require_once 'app/class/core/Language.php';
 require_once 'app/class/core/Medoo.php';
 require_once 'app/class/core/Fpdf.php';
+require_once 'app/class/core/Pdf.php';
 require_once 'app/class/Helper.php';
 
 include_once('config.php');
