7c64cd08a542bf064fa5d617fcabda85200937f8
Vladislav Domin <vladislav@v-d.sk>
2020-06-04 16:14:01 +0200

    api

--- api.php
+++ api.php
@@ -79,6 +79,102 @@ if(!empty($_GET)) {
         ]);
         return;
     }
+    else if($_GET['model'] == "suspension") {
+        if(!isset($_GET['wheel']) || !(isset($_GET['car'])) || !(isset($_GET['r']))) {
+            http_response_code(422);
+            echo json_encode([
+                'message' => 'Not all parameters were set.',
+            ]);
+            return;
+        }
+        $out = trim(shell_exec("octave --no-gui --quiet shell/kyvadlo.txt {$_GET['r']} {$_GET['angle']} {$_GET['position']}"));
+        $arr = preg_split("/\\r\\n|\\r|\\n/", trim(explode("ANGLE", $out)[0]));
+        $arr2 = preg_split("/\\r\\n|\\r|\\n/", trim(explode("ANGLE", $out)[1]));
+        $init = 0.0;
+        $pos = [];
+        $angle = [];
+        for($i = 0; $i < sizeof($arr); $i++) {
+            $pos[] = [
+                'y' => (float)trim($arr[$i]),
+                'x' => $init
+            ];
+            $angle[] = [
+                'y' => (float)trim($arr2[$i]),
+                'x' => $init,
+            ];
+            $init += 0.05;
+        }
+        http_response_code(200);
+        echo json_encode([
+            'position' => $pos,
+            'angle' => $angle,
+        ]);
+        return;
+    }
+    if($_GET['model'] == "ballbeam") {
+        if(!isset($_GET['angle']) || !(isset($_GET['position'])) || !(isset($_GET['r']))) {
+            http_response_code(422);
+            echo json_encode([
+                'message' => 'Not all parameters were set.',
+            ]);
+            return;
+        }
+        $out = trim(shell_exec("octave --no-gui --quiet shell/kyvadlo.txt {$_GET['r']} {$_GET['angle']} {$_GET['position']}"));
+        $arr = preg_split("/\\r\\n|\\r|\\n/", trim(explode("ANGLE", $out)[0]));
+        $arr2 = preg_split("/\\r\\n|\\r|\\n/", trim(explode("ANGLE", $out)[1]));
+        $init = 0.0;
+        $pos = [];
+        $angle = [];
+        for($i = 0; $i < sizeof($arr); $i++) {
+            $pos[] = [
+                'y' => (float)trim($arr[$i]),
+                'x' => $init
+            ];
+            $angle[] = [
+                'y' => (float)trim($arr2[$i]),
+                'x' => $init,
+            ];
+            $init += 0.05;
+        }
+        http_response_code(200);
+        echo json_encode([
+            'position' => $pos,
+            'angle' => $angle,
+        ]);
+        return;
+    }
+    if($_GET['model'] == "aircraftpitch") {
+        if(!isset($_GET['angle']) || !(isset($_GET['position'])) || !(isset($_GET['r']))) {
+            http_response_code(422);
+            echo json_encode([
+                'message' => 'Not all parameters were set.',
+            ]);
+            return;
+        }
+        $out = trim(shell_exec("octave --no-gui --quiet shell/kyvadlo.txt {$_GET['r']} {$_GET['angle']} {$_GET['position']}"));
+        $arr = preg_split("/\\r\\n|\\r|\\n/", trim(explode("ANGLE", $out)[0]));
+        $arr2 = preg_split("/\\r\\n|\\r|\\n/", trim(explode("ANGLE", $out)[1]));
+        $init = 0.0;
+        $pos = [];
+        $angle = [];
+        for($i = 0; $i < sizeof($arr); $i++) {
+            $pos[] = [
+                'y' => (float)trim($arr[$i]),
+                'x' => $init
+            ];
+            $angle[] = [
+                'y' => (float)trim($arr2[$i]),
+                'x' => $init,
+            ];
+            $init += 0.05;
+        }
+        http_response_code(200);
+        echo json_encode([
+            'position' => $pos,
+            'angle' => $angle,
+        ]);
+        return;
+    }
 }
 else {
     http_response_code(401);
